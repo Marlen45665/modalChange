6a4d17ca25f87232153b7896de4758b587c261d1
Марлен Девлетов <marlen@MacBook-Pro-Marlen-2.local>
2024-11-13 18:14:17 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { Acordion } from "./Acordion";
 const LayerTask = styled.div`
   padding: 18px 11px;
   width: 381px;
-  height: 306px;
+  /* height: 306px; */
   position: absolute;
-  border-radius: 6px;
+  /* border-radius: 6px; */
   background: white;
   box-shadow: 0px 10px 30px 0px rgba(0, 0, 0, 0.20),
     0px 30px 70px 0px rgba(26, 34, 64, 0.15),
